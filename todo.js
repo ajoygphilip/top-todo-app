23285f0e74a7619adcd03c6573b508718d1e7a1e
ajoygp <41632101+ajoygp@users.noreply.github.com>
2022-10-05 11:43:28 +0530

    Comment out advanced properties for ease

--- todo.js
+++ todo.js
@@ -2,13 +2,13 @@ class Todo{
     constructor(dueDate,title,description,priority,tags){
         this.dueDate=dueDate;
         this.title=title?title:"Untitled";
-        this.description=description?description:"";
-        this.priority=priority;
-        this.tags=tags;
+        // this.description=description?description:"";
+        // this.priority=priority;
+        // this.tags=tags;
     }
 
 
 
 }
 
-export {Todo};
+export { Todo };
