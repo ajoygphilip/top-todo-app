e2deeddac0bdfaadea365197239acae4c0efe511
ajoygp <41632101+ajoygp@users.noreply.github.com>
2022-10-05 11:31:25 +0530

    Create todo class

---
+++ todo.js
@@ -0,0 +1,14 @@
+class Todo{
+    constructor(dueDate,title,description,priority,tags){
+        this.dueDate=dueDate;
+        this.title=title?title:"Untitled";
+        this.description=description?description:"";
+        this.priority=priority;
+        this.tags=tags;
+    }
+
+
+
+}
+
+export {Todo}
