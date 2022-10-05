6c5b1a25cdb8a24fd1bc49f69763689498528b9c
ajoygp <41632101+ajoygp@users.noreply.github.com>
2022-10-05 14:52:46 +0530

    Implement add todo item functionality

--- script.js
+++ script.js
@@ -3,19 +3,31 @@ import { Todo } from "./todo.js";
 let addButton=document.getElementById("add-button");
 let popup= document.getElementById("create-form");
 let cancelButton= document.getElementById("cancel-button");
+let createButton= document.getElementById("create-button");
+
+
+
 const todos=[];
-let itemsElm=document.querySelector(".items")
+const itemsElm=document.querySelector(".items")
 
 function togglePopup(){
     popup.classList.toggle("hide");
 }
+
 addButton.addEventListener("click",togglePopup);
 
 cancelButton.addEventListener("click",togglePopup);
 
-todos.push(new Todo(Date.now(),"First item","",""));
+document.getElementById('datepicker').valueAsDate = new Date();
 
 function renderTodo(items){
+    if(items.length==0){
+        let h3=document.createElement("h5");
+        h3.innerText="You are all caught up!"
+        document.querySelector(".items").appendChild(h3)
+    }
+
+    itemsElm.innerHTML="";
     items.forEach(item => {
         let row= document.createElement("div");
         row.classList.add("row");
@@ -42,3 +54,16 @@ function renderTodo(items){
 };
 
 renderTodo(todos);
+
+createButton.addEventListener("click",createItem)
+
+function createItem(e){
+    // const category = document.querySelector("#categorypicker").value;
+    const date = document.querySelector("#datepicker").value;
+    const title = document.querySelector("#itemtitle").value;
+    todos.push(new Todo(date,title));
+    document.querySelector("#itemtitle").value="";
+    togglePopup();
+    renderTodo(todos);
+     
+}
