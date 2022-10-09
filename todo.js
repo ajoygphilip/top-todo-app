02208cc0b873eb63f88d9cfd7215869496615d51
ajoygp <41632101+ajoygp@users.noreply.github.com>
2022-10-09 11:28:50 +0530

    Add isComplete to todo class

--- todo.js
+++ todo.js
@@ -1,7 +1,8 @@
 export default class Todo{
-    constructor(dueDate,title,description,priority,tags){
+    constructor(dueDate,title,priority,tags){
         this.dueDate=dueDate;
         this.title=title || "Untitled";
+        this.isCompleted=false;
         // this.description=description?description:"";
         // this.priority=priority;
         // this.tags=tags;
